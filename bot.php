9d5eb3a3cd1ce7f92eee22d313403785776f457e
Sahasawas(Ham) <39586104+21hamy@users.noreply.github.com>
2021-01-29 15:05:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,9 +19,12 @@ if ( sizeof($request_array['events']) > 0 ) {
       $reply_message = '';
       $reply_token = $event['replyToken'];
       $text = $event['message']['text'];
+      if ($text == 'Hi') {
+         $text2 = 'hello mannn'
+      }
       $data = [
          'replyToken' => $reply_token,
-         'messages' => [['type' => 'text', 'text' => $text ]]
+         'messages' => [['type' => 'text', 'text' => $text2 ]]
       ];
       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
       $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
